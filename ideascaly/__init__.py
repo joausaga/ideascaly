780d1fa408677994c739ce489bd0fde2ed6242f0
joausaga <jorgesaldivar@gmail.com>
2015-04-01 12:31:00 +0200

    Add details of the project

--- ideascaly/__init__.py
+++ ideascaly/__init__.py
@@ -1,1 +1,17 @@
-__author__ = 'jorgesaldivar'
+# IdeaScaly
+# Copyright 2015 Jorge Saldivar
+# See LICENSE for details.
+
+"""
+IdeaScaly: IdeaScale API client
+"""
+
+__version__ = '0.1'
+__author__ = 'Jorge Saldivar'
+__license__ = 'MIT'
+
+from ideascaly.api import API
+from ideascaly.auth import AuthNonSSO, AuthNonSSOMem, AuthSSO, AuthResearch
+from ideascaly.error import IdeaScalyError
+from ideascaly.models import Author, Idea, Campaign, Comment, Vote, JSONModel, ModelFactory, Model
+from ideascaly.parsers import Parser, RawParser, JSONParser, ModelParser
