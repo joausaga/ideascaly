3d2b4536803df4a202d8c1c9b5d0e689f1053378
joausaga <jorgesaldivar@gmail.com>
2015-04-02 07:57:26 +0200

    Read token from environment variable

--- tests/config.py
+++ tests/config.py
@@ -1,13 +1,13 @@
+import os
 import sys
+import unittest
 sys.path.append('../ideascaly')
 
 from ideascaly.auth import AuthNonSSO
 from ideascaly.api import API
 
-import unittest
-
 testing_community = 'fiveheads.ideascale.com'
-testing_token = '5b3326f8-50a5-419d-8f02-eef6a42fd61a'
+testing_token = os.environ.get('TOKEN', '')
 
 
 class IdeascalyTestCase(unittest.TestCase):
